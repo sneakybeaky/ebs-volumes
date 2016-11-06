b815ba76c9fb6cf8df2524fac77e909e9a9a5f5d
Jon Barber <jon.barber@acm.org>
2016-11-06 22:43:52 +0000

    Listing the volumes attached

--- cmd/instanceid/main.go
+++ cmd/instanceid/main.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/sneakybeaky/aws-volumes/shared"
 	"log"
 )
+
+func findAttachedVolumesFor(session *session.Session, instanceid string) ([]*ec2.Volume, error) {
+
+	svc := ec2.New(session)
+
+	params := &ec2.DescribeVolumesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("attachment.instance-id"),
+				Values: []*string{
+					aws.String(instanceid),
+				},
+			},
+		},
+	}
+
+	resp, err := svc.DescribeVolumes(params)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Volumes, nil
+
+}
+
+func detachVolumes(volumes []*ec2.Volume) {
+	fmt.Println("Following volumes are attached")
+	for volume := range volumes {
+		fmt.Println(volume)
+	}
+}
 
 func main() {
 	sess, err := session.NewSession()
@@ -21,5 +55,20 @@ func main() {
 		log.Fatalf("failed to get instance id %v\n", err)
 	}
 	fmt.Printf("%s\n", id)
+
+	ec2Region := shared.NewEC2Region(sess)
+	region,err := ec2Region.Region()
+	if err != nil {
+		log.Fatalf("failed to get region %v\n", err)
+	}
+
+	sess.Config.Region = &region
+	volumes, err := findAttachedVolumesFor(sess, id)
+
+	if err != nil {
+		log.Fatalf("failed to find attached volumes %v\n", err)
+	}
+
+	detachVolumes(volumes)
 
 }

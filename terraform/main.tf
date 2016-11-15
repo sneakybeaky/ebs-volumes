a2a827b1b0c40796b3e41517651a6e17c7d3f51c
Jon Barber <jon.barber@acm.org>
2016-11-15 22:38:15 +0000

    Rudimentary attachment

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,7 @@ resource "aws_subnet" "default" {
   vpc_id = "${aws_vpc.default.id}"
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone = "${var.aws_availability_zone}"
 }
 
 

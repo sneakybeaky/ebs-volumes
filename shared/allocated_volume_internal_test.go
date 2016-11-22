15eb300b2dc231dc079944e7b31e9cc2a6485a58
Jon Barber <jon.barber@acm.org>
2016-11-22 17:50:55 +0000

    Fixed lint errors

--- shared/allocated_volume_internal_test.go
+++ shared/allocated_volume_internal_test.go
@@ -1,12 +1,12 @@
 package shared
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/sneakybeaky/aws-volumes/shared/internal/helper"
 	"github.com/aws/aws-sdk-go/private/waiter"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"fmt"
+	"github.com/sneakybeaky/aws-volumes/shared/internal/helper"
 )
 
 func TestDetachVolumeWhenAttached(t *testing.T) {
@@ -20,7 +20,7 @@ func TestDetachVolumeWhenAttached(t *testing.T) {
 	revert := waitForVolumeIDSucceeds(expectedVolumeID)
 	defer revert()
 
-	underTest := NewAllocatedVolume("vol-54321","/dev/sdg","i-11223344",mockEC2Service)
+	underTest := NewAllocatedVolume("vol-54321", "/dev/sdg", "i-11223344", mockEC2Service)
 
 	err := underTest.Detach()
 
@@ -33,14 +33,13 @@ func waitForVolumeIDSucceeds(volumeID string) func() {
 
 	saved := invokeWait
 
-	invokeWait = func(waiter waiter.Waiter) error{
+	invokeWait = func(waiter waiter.Waiter) error {
 		input := waiter.Input.(*ec2.DescribeVolumesInput)
 
 		if *input.VolumeIds[0] != volumeID {
-			return fmt.Errorf("I was expecting %s but got %s",volumeID,*input.VolumeIds[0])
-		} else {
-			return nil
+			return fmt.Errorf("I was expecting %s but got %s", volumeID, *input.VolumeIds[0])
 		}
+		return nil
 
 	}
 

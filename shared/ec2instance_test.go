bb62f5f15804e0945a5d9980670802baf8f6a2f9
Jon Barber <jon.barber@acm.org>
2016-11-22 16:21:36 +0000

    removed commented out code

--- shared/ec2instance_test.go
+++ shared/ec2instance_test.go
@@ -39,38 +39,3 @@ func assertVolumesEqual(t *testing.T, left *shared.AllocatedVolume, right *share
 	}
 }
 
-/*func TestAttachAllocatedVolumes(t *testing.T) {
-
-	metadata := &MockMetadata{instanceId: "id-98765", region: "erewhon"}
-
-	mockEC2Service := &MockEC2Service{
-		DescribeTagsFunc: describeVolumeTagsForInstance("id-98765",
-			NewDescribeTagsOutputBuilder().WithVolume("/dev/sda", "id-98765", "vol-1234567").WithVolume("/dev/sdb", "id-98765", "vol-54321").Build()),
-	}
-
-	var underTest = NewEC2Instance(metadata, mockEC2Service)
-
-	saved := attachVolume
-	defer func() {
-		attachVolume = saved
-	}()
-
-	set := make(map[string]struct{}, 2)
-	attachVolume = func(volume *AllocatedVolume) {
-		set[volume.VolumeId] = struct{}{}
-	}
-
-	underTest.AttachVolumes()
-
-	if len(set) != 2 {
-		t.Errorf("Should have been 2 volumes attached, but %d were", len(set))
-	}
-
-	expectedVolumes := []string{"vol-1234567", "vol-54321"}
-	for _, expectedVolume := range expectedVolumes {
-		if _, ok := set[expectedVolume]; !ok {
-			t.Errorf("Volume %s should have been attached, but wasn't", expectedVolume)
-		}
-	}
-
-}*/

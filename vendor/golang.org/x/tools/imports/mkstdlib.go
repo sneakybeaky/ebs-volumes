2479ae29b121ad2e46be791011650367c8cac1b8
Jon Barber <jon.barber@acm.org>
2017-03-27 08:48:16 +0100

    Refreshed vendored deps

--- vendor/golang.org/x/tools/imports/mkstdlib.go
+++ vendor/golang.org/x/tools/imports/mkstdlib.go
@@ -52,7 +52,6 @@ func main() {
 		mustOpen(api("go1.5.txt")),
 		mustOpen(api("go1.6.txt")),
 		mustOpen(api("go1.7.txt")),
-		mustOpen(api("go1.8.txt")),
 	)
 	sc := bufio.NewScanner(f)
 	fullImport := map[string]string{} // "zip.NewReader" => "archive/zip"

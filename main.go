0ae704ab25c289341198561f47609af61a9d52ff
Jon Barber <jbarber@thoughtworks.com>
2017-03-29 20:17:52 +0100

    Removed functions no longer used

--- main.go
+++ main.go
@@ -2,26 +2,8 @@ package main
 
 import (
 	"github.com/sneakybeaky/ebs-volumes/cmd"
-	"github.com/sneakybeaky/ebs-volumes/shared"
-	"github.com/sneakybeaky/ebs-volumes/shared/log"
 )
 
-var showInfo = func(instance *shared.EC2Instance) {
-	instance.ShowVolumesInfo()
-}
-
-var doAttach = func(instance *shared.EC2Instance) {
-	instance.AttachVolumes()
-}
-
-var doDetach = func(instance *shared.EC2Instance) {
-	if err := instance.DetachVolumes(); err != nil {
-		log.Error.Fatalf("Unable to detach volumes : %v\n", err)
-	}
-}
-
 func main() {
-
 	cmd.Execute()
-
 }

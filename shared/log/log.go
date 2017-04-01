f5c235ee6eaa7d600a7ded63fe803f830eedef9f
Jon Barber <jon.barber@acm.org>
2017-04-01 15:30:52 +0100

    Fixes for golint warnings

--- shared/log/log.go
+++ shared/log/log.go
@@ -6,12 +6,15 @@ import (
 	"os"
 )
 
+// Debug, Info and Error are used for logging at the appropriate level
+
 var (
 	Debug = log.New(ioutil.Discard, "", 0)
 	Info  = log.New(os.Stdout, "", 0)
 	Error = log.New(os.Stdout, "", 0)
 )
 
+// SetVerbose turns on logging categories for verbose logging
 func SetVerbose() {
 	Debug.SetOutput(os.Stdout)
 }

3bff6396f8b7f2fc43712fc8703b557fe10d8d30
Jon Barber <jon.barber@acm.org>
2017-04-03 21:26:55 +0100

    Test for attach command bubbling up errors

--- cmd/attach.go
+++ cmd/attach.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/sneakybeaky/ebs-volumes/shared"
 	"github.com/spf13/cobra"
 )
@@ -12,17 +10,10 @@ var attachCmd = &cobra.Command{
 	Short: "Attach volumes",
 	Long:  `Attaches volumes designated via tags`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return attach()
+		return apply(attachVolumes)
 	},
 }
 
-func attach() error {
-
-	instance, err := shared.GetInstance()
-
-	if err != nil {
-		return fmt.Errorf("unable to get EC2 instance : %v", err)
-	}
-
+func attachVolumes(instance *shared.EC2Instance) error {
 	return instance.AttachVolumes()
 }
